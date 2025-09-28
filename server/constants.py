e53d8ee855bd959e749e474ff2d191b97855faa0
Yixuan Huang <yixuanhuang98@gmail.com>
2025-09-28 13:00:00 -0400

    first commit from server side

--- server/constants.py
+++ server/constants.py
@@ -23,21 +23,21 @@ MARKER_IDS = [10, 13, 17, 21, 23, 30, 37, 41, 42, 45, 48, 49]
 # Camera
 
 CAMERA_SERIALS = [
-    'E4298F4E',  # Top camera
-    '099A11EE',  # Bottom camera
+    '515A41BE',  # Top camera
+    'A01861BE',  # Bottom camera
 ]
 CAMERA_FOCUS = 0
 CAMERA_TEMPERATURE = 3900
 CAMERA_EXPOSURE = 77  # 77 is best, 156 is slightly worse, 312 results in motion blur
 CAMERA_GAIN = 50  # Increments of 10
-CAMERA_HEIGHT = 2.71  # 271 cm
+CAMERA_HEIGHT = 2.6  # 271 cm
 PIXELS_PER_M = 500
 
 ################################################################################
 # Floor
 
 NUM_FLOOR_TILES_X = 6
-NUM_FLOOR_TILES_Y = 7
+NUM_FLOOR_TILES_Y = 6
 FLOOR_TILE_SIZE = 24 * 0.0254  # 2 ft
 FLOOR_LENGTH = NUM_FLOOR_TILES_Y * FLOOR_TILE_SIZE
 FLOOR_WIDTH = NUM_FLOOR_TILES_X * FLOOR_TILE_SIZE
@@ -46,6 +46,6 @@ FLOOR_WIDTH = NUM_FLOOR_TILES_X * FLOOR_TILE_SIZE
 # Robot
 
 NUM_ROBOTS = 3
-ROBOT_WIDTH = 0.465  # 46.5 cm
-ROBOT_HEIGHT = 0.325  # 32.5 cm
-ROBOT_DIAG = 0.55  # 55 cm
+ROBOT_WIDTH = 0.500  # 50.0 cm
+ROBOT_HEIGHT = 0.378  # 37.8 cm
+ROBOT_DIAG = 0.707  # 70.7 cm
